e74dfa9875d890592cf0ea373181c5e8e237aba9
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-01-13 09:34:34 +0200

    feat: add function to generate big file with test data

---
+++ createHugeJSON.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import { v4 as uuid } from 'uuid';
+import { User } from './types';
+
+type TestUser = Pick<User, 'id' | 'first_name'>;
+
+const file = fs.createWriteStream('./huge.json');
+const NUMBER_OF_USERS = 1e6;
+
+for (let i = 0; i <= NUMBER_OF_USERS; i++) {
+    if (i === 0) {
+        file.write('[\n');
+    }
+
+    const user: TestUser = {
+        id: i,
+        first_name: uuid()
+    };
+
+    if (i === NUMBER_OF_USERS - 1) {
+        file.write(`  ${JSON.stringify(user)}\n`);
+        file.write(']');
+        break;
+    }
+    file.write(`  ${JSON.stringify(user)},\n`);
+}
+
+file.end();
