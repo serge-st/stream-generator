3b5a9364e7f3b22a3866885c437a7247bad4d664
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-01-13 19:08:43 +0200

    feat: improve stream`s stability

--- getAllInOneArray.ts
+++ getAllInOneArray.ts
@@ -5,8 +5,17 @@ import { getAllUsers } from './usersGenerator';
 export async function getAllInOneArray(res: ServerResponse): Promise<void> {
     const usersStream = new UserTransform();
     const users = getAllUsers();
+    let waitingForDrain = false;
+
     for await (const chunk of users) {
-        usersStream.write(chunk);
+        const shouldContinue = usersStream.write(chunk);
+        if (!shouldContinue && !waitingForDrain) {
+            waitingForDrain = true;
+            await new Promise<void>(resolve => usersStream.once('drain', () => {
+                waitingForDrain = false;
+                resolve();
+            }));
+        }
     }
     usersStream.end();
 
