850a650c81e7e8b2e1138960ed518df9111070f2
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-01-16 21:16:59 +0200

    feat: improve memory allocation

--- getAllInOneArray.ts
+++ getAllInOneArray.ts
@@ -6,9 +6,10 @@ export async function getAllInOneArray(res: ServerResponse): Promise<void> {
     const usersStream = new UserTransform();
     const users = getAllUsers();
     let waitingForDrain = false;
+    let shouldContinue = true;
 
     for await (const chunk of users) {
-        const shouldContinue = usersStream.write(chunk);
+        shouldContinue = usersStream.write(chunk);
         if (!shouldContinue && !waitingForDrain) {
             waitingForDrain = true;
             await new Promise<void>(resolve => usersStream.once('drain', () => {
