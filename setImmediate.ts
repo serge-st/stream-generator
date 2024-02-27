2f5d35e9f0da3bf918e6f3e76563a708acb7f7cc
Serge Stecenko <sergejs.stecenko@gmail.com>
2024-02-27 20:48:05 +0200

    feat: add colerful logs

--- setImmediate.ts
+++ setImmediate.ts
@@ -1,3 +1,5 @@
+import chalk from 'chalk';
+
 // * Event Loop sequence:
 
 // * 1. Call stack
@@ -6,31 +8,31 @@
 // * ^^^ Repeat
 
 // * Sync callstack
-console.log('log 1');
+console.log(chalk.blue('sync'), chalk.dim('log 1'));
 // * Macrotask queue
-setImmediate(() => console.log('log 2'));
+setImmediate(() => console.log(chalk.dim.cyan('setImmediate'), chalk.yellow('macrotask'), chalk.dim('log 2')));
 // * Macrotask queue
 setTimeout(() => {
-    console.log('log 3 setTimeout 0ms')
+    console.log(chalk.blue('sync'), 'inside', chalk.yellow('macrotask'), chalk.dim('log 3 setTimeout 0ms'))
     // * Microtask queue -> Immediately after log 3
-    new Promise<void>(res => res()).then(() => console.log('log 4'))
-    // * Microtask queue -> Immediately after log 4
-    new Promise<void>(res => res()).then(() => console.log('log 5'))
+    new Promise<void>(res => res()).then(() => console.log(chalk.dim.cyan('Promise'), chalk.cyan('microtask'), 'inside', chalk.yellow('macrotask'), chalk.dim('log 4')))
+    // * Microtask queue -> Immediately after log 4)
+    new Promise<void>(res => res()).then(() => console.log(chalk.dim.cyan('Promise'), chalk.cyan('microtask'), 'inside', chalk.yellow('macrotask'), chalk.dim('log 5')))
     // * Macrotask queue
-    setTimeout(() => console.log('log 6 setTimeout 1ms'), 1);
+    setTimeout(() => console.log(chalk.dim.cyan('setTimeout'), chalk.yellow('macrotask'), 'inside', chalk.yellow('macrotask'), chalk.dim('log 6 setTimeout 1ms')), 1);
 }, 0);
 // * Microtask queue
-queueMicrotask(() => console.log('log 7'))
+queueMicrotask(() => console.log(chalk.dim.cyan('queueMicrotask'), chalk.cyan('microtask'), chalk.dim('log 7')))
 // * Macrotask queue
-setTimeout(() => console.log('log 8 setTimeout 1ms'), 1);
+setTimeout(() => console.log(chalk.dim.cyan('setTimeout'), chalk.yellow('macrotask'), chalk.dim('log 8 setTimeout 1ms')), 1);
 // * Macrotask queue
-setImmediate(() => console.log('log 9'));
+setImmediate(() => console.log(chalk.dim.cyan('setImmediate'), chalk.yellow('macrotask'), chalk.dim('log 9')));
 // * Microtask queue
-new Promise<void>(res => res()).then(() => console.log('log 10'))
+new Promise<void>(res => res()).then(() => console.log(chalk.dim.cyan('Promise'), chalk.cyan('microtask'), chalk.dim('log 10')))
 // * Next tick queue -> Immediately after Loop 1 call stack is empty
-process.nextTick(() => console.log('log 11'));
+process.nextTick(() => console.log(chalk.magenta('process.nextTick'), chalk.dim('log 11')));
 // * Sync callstack
-console.log('log 12')
+console.log(chalk.blue('sync'), chalk.dim('log 12'))
 
 // * A way to execute some piece of code asynchronously, but as soon as possible.
 // * Any function passed as the setImmediate() argument is a callback that's executed in the next iteration of the event loop .
